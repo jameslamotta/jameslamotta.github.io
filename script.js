55258ff8f852bda575d468599029c0cb890bb6eb
Jaime Perujo <jaimeperujo@gmail.com>
2018-10-09 14:21:44 +0200

    y

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function gtag() {dataLayer.push(arguments)}
 
 function implementExperimentA(value) {
   if (value ==  '0') {
-    document.getElementById("section1").innerHTML = "Hello World!";
+    document.getElementById("section2").innerHTML = "Hello World!";
   } else if (value == '1') {
-    document.getElementById("section1").innerHTML = "holaaaaa 2222 !";
+    document.getElementById("section2").innerHTML = "holaaaaa 2222 !";
   } else if (value == '2') {
-    document.getElementById("section1").innerHTML = "holaaaa 33333 !";
+    document.getElementById("section2").innerHTML = "holaaaa 33333 !";
   }
 }
 
