fa878914dff78fb91dfc88be37fcf237bc85a740
Jaime Perujo <jaimeperujo@gmail.com>
2018-10-23 18:51:20 +0200

    dfh
    sgdf

--- script.js
+++ script.js
@@ -5,17 +5,17 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() {dataLayer.push(arguments);}
 
-function implementExperimentA(value) {
+function implementExperimentCopy(value) {
   if (value ==  '0') {
     document.getElementById("section2").innerHTML = "(Original) Jaime Perujo testing site";
   } else if (value == '1') {
-    document.getElementById("section2").innerHTML = "Jaime's personal LAB :)";
+    document.getElementById("section2").innerHTML = "Copy 1: Jaime's personal LAB :)";
   } else if (value == '2') {
-    document.getElementById("section2").innerHTML = "This is a test, it will be self-destroyed in 3,2,1...";
+    document.getElementById("section2").innerHTML = "Copy 2: This is a test, it will be self-destroyed in 3,2,1...";
   }
 }
 
 gtag('event', 'optimize.callback', {
     name: 'NRLruprYTsCigOEgkwVGqA',
-    callback: implementExperimentA
+    callback: implementExperimentCopy
  });
