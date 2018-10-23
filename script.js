7295849c5736ae6d79adc4da098b9d69c12a7036
Jaime Perujo <jaimeperujo@gmail.com>
2018-10-23 18:33:54 +0200

    news
    vfas

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function gtag() {dataLayer.push(arguments);}
 
 function implementExperimentA(value) {
   if (value ==  '0') {
-    document.getElementById("section2").innerHTML = "Hello World!";
+    document.getElementById("section2").innerHTML = "(Original) Jaime Perujo testing site";
   } else if (value == '1') {
-    document.getElementById("section2").innerHTML = "holaaaaa 2222 !";
+    document.getElementById("section2").innerHTML = "Jaime's personal LAB :)";
   } else if (value == '2') {
-    document.getElementById("section2").innerHTML = "holaaaa 33333 !";
+    document.getElementById("section2").innerHTML = "This is a test, it will be self-destroyed in 3,2,1...";
   }
 }
 
