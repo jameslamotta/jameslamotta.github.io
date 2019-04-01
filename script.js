2028029b35a7e71eb2abcdba4e84b31252c3d227
Jaime Perujo <jaimeperujo@gmail.com>
2019-04-01 14:06:35 +0200

    dfg
    gfd

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {dataLayer.push(arguments);}
 
 
-
+/*
 
 function implementExperimentCopy(value) {
  implementExperimentTheme(value);
@@ -39,6 +39,7 @@ function implementExperimentTheme(value) {
   }
 }
 
+*/
 
 
 
